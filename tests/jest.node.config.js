c9537f3daa334883954460289e0beabaa7b27c2a
Efra Peralta <efra23peralta@gmail.com>
2022-07-06 19:58:56 -0400

    chore: add coverage to jest config

--- tests/jest.node.config.js
+++ tests/jest.node.config.js
@@ -1,15 +1,22 @@
 module.exports = {
   preset: 'ts-jest',
   roots: ['<rootDir>'],
-  collectCoverageFrom: ['<rootDir>/**/*.{js,ts}'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js,ts}', '!**/node_modules/**'],
+  /**
+   * @todo add coverage threshold
+   *
+   * @see https://jestjs.io/docs/configuration#coveragethreshold-object
+   */
   verbose: true,
   bail: true,
   testMatch: [
     '<rootDir>/tests/**/?(*.)+(spec|test).[tj]s?(x)',
-    '**/?(*.)+(spec|test).[tj]s?(x)'
+    '**/?(*.)+(spec|test).[tj]s?(x)',
   ],
   moduleDirectories: ['node_modules', 'src'],
   setupFiles: [
-    '<rootDir>/node_modules/@turnly/eslint-config/tests/jest.setup.js'
+    '<rootDir>/node_modules/@turnly/eslint-config/tests/jest.setup.js',
   ],
 }
